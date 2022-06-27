a8a4d420a2b973f709290d2507209ea19338497f
alicalimli <alicalimli76@gmail.com>
2022-06-27 10:43:25 +0300

    added icon in conversation footer

--- src/Conversation.jsx
+++ src/Conversation.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { IoIosSend } from "react-icons/io";
+
 const Conversation = () => {
   return (
     <div className="h-screen w-screen p-4 flex justify-center">
@@ -12,14 +14,16 @@ const Conversation = () => {
         </header>
 
         <footer className="mt-auto bg-white rounded-xl w-full h-16 p-2 flex items-center">
-          <form action="#" className="w-full flex items-center">
+          <form action="#" className="w-full flex items-center gap-1">
             <input
               required
               type="text"
               placeholder="Message here"
-              className="p-2 px-4"
+              className="p-2 px-4 w-full"
             />
-            <button className="rounded-xl ml-auto w-12 h-12 bg-blue-600"></button>
+            <button className="rounded-xl ml-auto w-12 h-12 bg-blue-600 flex items-center justify-center">
+              <IoIosSend className="text-white text-2xl" />
+            </button>
           </form>
         </footer>
       </div>
