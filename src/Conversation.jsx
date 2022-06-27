e9f006c354bd26b19d3319d824936057889aa5be
alicalimli <alicalimli76@gmail.com>
2022-06-27 10:48:41 +0300

    added main body in conversation.jsx

--- src/Conversation.jsx
+++ src/Conversation.jsx
@@ -5,15 +5,8 @@ import { IoIosSend } from "react-icons/io";
 const Conversation = () => {
   return (
     <div className="h-screen w-screen p-4 flex justify-center">
-      <div className="w-1/2 flex flex-col">
-        <header className="bg-white rounded-xl w-full h-20 p-2 px-4 flex items-center">
-          <div className="flex items-center gap-4">
-            <div className="bg-blue-600 h-12 w-12 rounded-full"></div>
-            <h2 className="text-2xl text-black">Elvis</h2>
-          </div>
-        </header>
-
-        <footer className="mt-auto bg-white rounded-xl w-full h-16 p-2 flex items-center">
+      <div className="w-1/2 flex flex-col-reverse gap-4">
+        <div className="bg-white rounded-xl w-full h-16 p-2 flex items-center">
           <form action="#" className="w-full flex items-center gap-1">
             <input
               required
@@ -21,11 +14,25 @@ const Conversation = () => {
               placeholder="Message here"
               className="p-2 px-4 w-full"
             />
-            <button className="rounded-xl ml-auto w-12 h-12 bg-blue-600 flex items-center justify-center">
+            <button className="rounded-xl ml-auto w-12 h-12 bg-blue-500 flex items-center justify-center">
               <IoIosSend className="text-white text-2xl" />
             </button>
           </form>
-        </footer>
+        </div>
+        <main className="flex flex-col gap-2">
+          <div className="flex bg-white text-black mr-auto p-2 px-4 w-fit rounded-xl">
+            <p className="text-lg">This is a text</p>
+          </div>
+          <div className="flex bg-blue-500 text-white ml-auto p-2 px-4 w-fit rounded-xl">
+            <p className="text-lg">This is a text</p>
+          </div>
+        </main>
+        <div className="bg-white rounded-xl w-full h-20 p-2 px-4 flex items-center mb-auto">
+          <div className="flex items-center gap-4">
+            <div className="bg-blue-500 h-12 w-12 rounded-full"></div>
+            <h2 className="text-2xl text-black">Elvis</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
