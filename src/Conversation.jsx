2738fc5d2a0333b9c36ffbc5aae6f5843d9fc227
alicalimli <alicalimli76@gmail.com>
2022-06-28 11:17:28 +0300

    animation adjustments

--- src/Conversation.jsx
+++ src/Conversation.jsx
@@ -61,10 +61,12 @@ const Conversation = () => {
           ref={conversationContainer}
           className="relative flex flex-col gap-2 overflow-scroll scrollbar-hide"
         >
-          {showArrowDown && (
+        <AnimatePresence>
+           {showArrowDown && (
             <motion.div
-              animate={{ opacity: 1, y: 0, x: "-50%" }}
-              initial={{ opacity: 0, y: 50, x: "-50%" }}
+              animate={{ opacity: 1,scale:1 ,y: "0%" ,  x: "-50%" }}
+              initial={{ opacity: 0,scale:0 ,y: "50%",   x: "-50%" }}
+              exit={{ opacity: 0,scale:0,y: "50%",  x: "-50%" }}
               className="fixed bottom-[12%] left-1/2 -translate-x-1/2"
             >
               <button
@@ -75,14 +77,16 @@ const Conversation = () => {
               </button>
             </motion.div>
           )}
+        </AnimatePresence>
+
           {/* <div className="flex bg-white text-black mr-auto p-2 px-4 w-fit rounded-xl">
             <p className="text-lg">This is a text</p>
           </div> */}
           {messages.map((currentMsg, i) => (
             <motion.div
               key={currentMsg + i}
-              animate={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1,x: "0%", opacity: 1 }}
+              initial={{ scale: 0,x: "100%", opacity: 0 }}
               ref={latestMsg}
               className="flex bg-blue-500 text-white ml-auto p-2 px-4 w-fit rounded-xl"
             >
