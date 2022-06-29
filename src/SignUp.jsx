ebe7f15921a7ba275c828a8df23135077628aca1
alicalimli <alicalimli76@gmail.com>
2022-06-29 10:29:13 +0300

    used InputForm component in Signup.jsx

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import InputForm from './InputForm.jsx'
+
 const SignUp = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center p-4">
@@ -8,37 +10,15 @@ const SignUp = () => {
         className="w-full sm:w-96 flex flex-col gap-4 p-12 rounded-xl bg-white shadow-lg "
       >
         <label className="text-2xl text-left">Sign Up</label>
-        <label htmlFor="email" className="flex flex-col gap-2 ">
 
-          Email
-          <input
-            type="text"
-            placeholder="e.g example@email.com"
-            required
-            id="email"
-            className="p-2 px-4 rounded-xl border"
-          />
-        </label>
-        <label htmlFor="username" className="flex flex-col gap-2 ">
-          Username
-          <input
-            type="text"
-            placeholder="e.g example123"
-            required
-            id="username"
-            className="p-2 px-4 rounded-xl border"
-          />
-        </label>
-        <label htmlFor="password" className="flex flex-col gap-2 ">
-          Password
-          <input
-            type="password"
-            placeholder="************"
-            required
-            id="username"
-            className="p-2 px-4 rounded-xl border"
-          />
-        </label>
+                <InputForm
+          label="Email"
+          type="email"
+          placeHolder="e.g example@email.com"
+        />
+        <InputForm label="Username" type="text" placeHolder="e.g example123" />
+        <InputForm label="Password" type="password" placeHolder="*********" />
+
         <button className="bg-blue-500 hover:bg-blue-400 duration-300 rounded-xl p-2 px-4 text-white">
           Sign Up
         </button>
