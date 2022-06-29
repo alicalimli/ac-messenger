bf0b6e52d70742bd44611948ce48c159ef8cd1ff
alicalimli <alicalimli76@gmail.com>
2022-06-29 17:07:18 +0300

    used useRef hook in login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,18 +1,22 @@
+import { useRef } from 'react'
 import { Link } from "react-router-dom";
 
 import InputForm from "./InputForm.jsx";
 
 const Login = () => {
+  const userNameRef = useRef();
+  const userPassRef = useRef();
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center p-4">
       <form
-        action="#"
+        onClick={handleLogin}
         className="w-full sm:w-96 flex flex-col gap-4 p-12 rounded-xl bg-white shadow-lg "
       >
         <label className="text-2xl text-left">Login</label>
 
-        <InputForm label="Username" type="text" placeHolder="e.g example123" />
-        <InputForm label="Password" type="password" placeHolder="*********" />
+        <InputForm label="Username" type="text" placeHolder="e.g example123" inputRef={userNameRef} />
+        <InputForm label="Password" type="password" placeHolder="*********" inputRef={userPassRef} />
 
         <button className="bg-blue-500 hover:bg-blue-400 duration-300 rounded-xl p-2 px-4 text-white">
           Login
