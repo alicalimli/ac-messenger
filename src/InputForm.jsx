1980a871046717f9d928f3d53db0f120ca91cb58
alicalimli <alicalimli76@gmail.com>
2022-06-29 10:42:39 +0300

    used UseRef to get values of inputs in signup.jsx

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,9 +1,10 @@
-const InputForm = ( {label, type, placeHolder} ) => {
+const InputForm = ( {label, type, placeHolder, inputRef} ) => {
 	return (
 		<label htmlFor={`${label}-input`} className="flex flex-col gap-2 ">
 			{label}
 			<input
 				type={type}
+				ref={inputRef}
 				placeholder={placeHolder}
 				id={label}
 				className="p-2 px-4 rounded-xl border"
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,12 +1,23 @@
+import {useState, useEffect, useRef} from 'react';
+
 import { Link } from "react-router-dom";
 
 import InputForm from "./InputForm.jsx";
 
 const SignUp = () => {
+  const userEmailRef = useRef(null)
+  const userNameRef = useRef(null)
+  const userpasswordRef = useRef(null)
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(userEmailRef.current)
+  }
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center p-4">
       <form
-        action="#"
+        onClick={handleSignUp}
         className="w-full sm:w-96 flex flex-col gap-4 p-12 rounded-xl bg-white shadow-lg "
       >
         <label className="text-2xl text-left">Sign Up</label>
@@ -15,9 +26,10 @@ const SignUp = () => {
           label="Email"
           type="email"
           placeHolder="e.g example@email.com"
+          inputRef={userEmailRef}
         />
-        <InputForm label="Username" type="text" placeHolder="e.g example123" />
-        <InputForm label="Password" type="password" placeHolder="*********" />
+        <InputForm label="Username" type="text" placeHolder="e.g example123" inputRef={userNameRef}/>
+        <InputForm label="Password" type="password" placeHolder="*********" inputRef={userpasswordRef} />
 
         <button className="bg-blue-500 hover:bg-blue-400 duration-300 rounded-xl p-2 px-4 text-white">
           Sign Up
