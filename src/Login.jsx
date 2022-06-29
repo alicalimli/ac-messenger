136f995b5ffcd881cfe8beede7f0f1118baa95be
alicalimli <alicalimli76@gmail.com>
2022-06-29 10:30:33 +0300

    used InputForm in login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import InputForm from "./InputForm.jsx";
+
 const Login = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center p-4">
@@ -8,26 +10,10 @@ const Login = () => {
         className="w-full sm:w-96 flex flex-col gap-4 p-12 rounded-xl bg-white shadow-lg "
       >
         <label className="text-2xl text-left">Login</label>
-        <label htmlFor="username" className="flex flex-col gap-2 ">
-          Username
-          <input
-            type="text"
-            placeholder="e.g example@email.com"
-            required
-            id="username"
-            className="p-2 px-4 rounded-xl border"
-          />
-        </label>
-        <label htmlFor="password" className="flex flex-col gap-2 ">
-          Password
-          <input
-            type="password"
-            placeholder="************"
-            required
-            id="username"
-            className="p-2 px-4 rounded-xl border"
-          />
-        </label>
+
+        <InputForm label="Username" type="text" placeHolder="e.g example123" />
+        <InputForm label="Password" type="password" placeHolder="*********" />
+
         <button className="bg-blue-500 hover:bg-blue-400 duration-300 rounded-xl p-2 px-4 text-white">
           Login
         </button>
