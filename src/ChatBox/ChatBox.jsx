0c1b30927a8272aa8aded8c6bf528a47d5e5abf4
alicalimli <alicalimli76@gmail.com>
2022-06-29 09:26:54 +0300

    format code

--- src/ChatBox/ChatBox.jsx
+++ src/ChatBox/ChatBox.jsx
@@ -8,6 +8,7 @@ import { BiMicrophone } from "react-icons/bi";
 import { RiImageAddLine } from "react-icons/ri";
 import { BiUser } from "react-icons/bi";
 
+
 import Messages from "../Messages/Messages.jsx";
 
 const ChatBox = () => {
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,13 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <form action="#" className="flex flex-col gap-4 p-20 rounded-xl bg-white shadow-md ">
-
-    <label className="text-2xl text-left">Login</label>
+      <form
+        action="#"
+        className="flex flex-col gap-4 p-20 rounded-xl bg-white shadow-lg "
+      >
+        <label className="text-2xl text-left">Login</label>
         <label htmlFor="username" className="flex flex-col gap-2 ">
           Username
           <input
@@ -27,7 +29,12 @@ const Login = () => {
         <button className="bg-blue-500 rounded-xl p-2 px-4 text-white">
           Login
         </button>
-        <p className="text-slate-600 text-sm">Don't have an account? <Link to="/signup" className="text-blue-500 font-semibold">Sign up</Link></p>
+        <p className="text-slate-600 text-sm">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500 font-semibold">
+            Sign up
+          </Link>
+        </p>
       </form>
     </div>
   );
