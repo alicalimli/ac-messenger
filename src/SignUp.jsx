c271be98a03ec33db07dc35aeb3f3d139af6b044
alicalimli <alicalimli76@gmail.com>
2022-06-30 11:44:20 +0300

    handling sign up form

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -7,17 +7,33 @@ import InputForm from "./InputForm.jsx";
 const SignUp = () => {
   const userEmailRef = useRef(null);
   const userNameRef = useRef(null);
-  const userpasswordRef = useRef(null);
+  const userPassRef = useRef(null);
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
-    console.log(userEmailRef.current);
+
+    if(!userNameRef.current.value && !userPassRef.current.value && !userEmailRef.current.value) {
+      console.log('Input your password and username')
+      return;
+    }
+
+    console.log("creatingUser")
+
+    const formData = {
+      "username": userNameRef.current.value,
+      "password": userPassRef.current.value,
+      "email": userEmailRef.current.value,
+    }
+
+    const createUser = await fetch("http://127.0.0.1:8000/api/v1/users/", { method: "POST", body: formData });
+
+    const res = await createUser.text();
   };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center p-4">
       <form
-        onClick={handleSignUp}
+        onSubmit={handleSignUp}
         className="w-full sm:w-96 flex flex-col gap-4 p-12 rounded-xl bg-white shadow-lg "
       >
         <label className="text-2xl text-left">Sign Up</label>
@@ -38,7 +54,7 @@ const SignUp = () => {
           label="Password"
           type="password"
           placeHolder="*********"
-          inputRef={userpasswordRef}
+          inputRef={userPassRef}
         />
 
         <button className="bg-blue-500 hover:bg-blue-400 duration-300 rounded-xl p-2 px-4 text-white">
