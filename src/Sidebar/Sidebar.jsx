ede470ab2ae2766c4c58f1d69fb163cd6abbf304
alicalimli <alicalimli76@gmail.com>
2022-07-01 17:13:45 +0300

    used UseContextProvider in sidebar

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,11 +1,17 @@
+import {useContext} from 'react'
+
+import {UserContext} from '../UserContextProvider/UserContextProvider'
+
 const Sidebar = () => {
+  const user = useContext(UserContext);
+  console.log(user[0])
   return (
     <nav className="p-4 w-72 bg-gray-300">
       <div className="flex gap-2">
         <div className="p-4 w-12 bg-red-600 rounded-full"></div>
         <div className="flex flex-col">
-          <p className="text-lg text-black font-semibold">ali_dev</p>
-          <p className="text-sm text-slate-500">alicalimli76@gmail.com</p>
+          <p className="text-lg text-black font-semibold">{user[0].userName}</p>
+          <p className="text-sm text-slate-500">{user[0].userEmail}</p>
         </div>
       </div>
     </nav>
