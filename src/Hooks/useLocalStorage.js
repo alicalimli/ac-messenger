df17037a4dc2cfa2b8d0806e7ddb2693bc4f7e7f
alicalimli <alicalimli76@gmail.com>
2022-07-01 18:53:01 +0300

    created useLocalStorage custom hook

---
+++ src/Hooks/useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedData, setStoredData] = useState(() => {
+    const data = JSON.parse(window.localStorage.getItem(key));
+
+    return data || initialValue;
+  });
+
+  const setValue = (value) => {
+    setStoredData(value);
+
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedData, setValue];
+};
+
+export default useLocalStorage;
