a9e4d666d423d1cb2b81d89399d24f2b2ca9ea48
alicalimli <alicalimli76@gmail.com>
2022-07-01 16:00:41 +0300

    temporary name and email

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,7 +1,13 @@
 const Sidebar = () => {
 	return (
-		<nav class="p-4 bg-gray-300">
-
+		<nav class="p-4 w-72 bg-gray-300">
+			<div className="flex gap-2">
+				<div className="p-4 w-12 bg-red-600 rounded-full"></div>
+				<div className="flex flex-col">
+					<p className="text-lg text-black font-semibold">ali_dev</p>
+					<p className="text-sm text-slate-500">alicalimli76@gmail.com</p>
+				</div>
+			</div>
 		</nav>
 		);
 }
