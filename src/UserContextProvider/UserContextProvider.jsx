a674eec24f7355bed475906164b507693d2c4888
alicalimli <alicalimli76@gmail.com>
2022-07-01 17:04:56 +0300

    mutated UserContext when signed up

--- src/UserContextProvider/UserContextProvider.jsx
+++ src/UserContextProvider/UserContextProvider.jsx
@@ -6,7 +6,7 @@ export const UserContext = createContext();
 export const UserContextProvider = ({ children }) => {
 	const [user, setUser] = useState({
 		userName: "",
-		userPass: "",
+		userPassword: "",
 		userEmail: "",
 	});
 
