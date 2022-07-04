f5fef6940a0826873bcd48010dc8716478bf078c
alicalimli <alicalimli76@gmail.com>
2022-07-04 08:17:17 +0300

    used authenticate.js in login

--- src/Authentication/Login/Login.jsx
+++ src/Authentication/Login/Login.jsx
@@ -7,6 +7,9 @@ import { UserContext } from "../../Contexts";
 
 import { useLocalStorage } from "../../Hooks";
 
+import { authenticate } from '../../Helpers'
+
+
 const Login = () => {
   const navigate = useNavigate();
   const userEmailRef = useRef();
@@ -18,34 +21,7 @@ const Login = () => {
     try {
       e.preventDefault();
 
-      let loginFormData = new FormData();
-      loginFormData.append("username", userEmailRef.current.value);
-      loginFormData.append("password", userPassRef.current.value);
-
-      const loginUser = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
-        method: "POST",
-        body: loginFormData,
-      });
-
-      const loginResults = await loginUser.json();
-
-      if (!loginResults.access_token)
-        throw new Error("Incorrect email or password");
-
-      // GETTING USER INFO FROM THE API
-
-      const userInfo = await fetch("http://127.0.0.1:8000/api/v1/users/me", {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + loginResults.access_token,
-        },
-      });
-
-      const userInfoRes = await userInfo.json();
-
-      setUser(Object.assign(user, userInfoRes.user))
-      setUserToken(loginResults.access_token);
-      navigate("/home");
+      authenticate();
     } catch (error) {
       setErrorMsg(error.message);
       console.error(error);
