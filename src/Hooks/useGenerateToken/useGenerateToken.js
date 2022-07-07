99a733a308537ced1784781367ece82033bb774e
alicalimli <alicalimli76@gmail.com>
2022-07-07 17:13:06 +0300

    format code

--- src/Hooks/useGenerateToken/useGenerateToken.js
+++ src/Hooks/useGenerateToken/useGenerateToken.js
@@ -1,30 +1,36 @@
 import React, { useCallback } from "react";
 
 const useGenerateToken = () => {
-  const generateToken = useCallback(async (setToken, email, pass, setErrorMsg) => {
-    try {
-      let formData = new FormData();
-      formData.append("username", email);
-      formData.append("password", pass);
+  const generateToken = useCallback(
+    async (setToken, email, pass, setErrorMsg) => {
+      try {
+        let formData = new FormData();
+        formData.append("username", email);
+        formData.append("password", pass);
 
-      // Request login from the API
-      const fetchToken = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
-        method: "POST",
-        body: formData,
-      });
+        // Request login from the API
+        const fetchToken = await fetch(
+          "http://127.0.0.1:8000/api/v1/auth/login",
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
 
-      const fetchTokenResults = await fetchToken.json();
+        const fetchTokenResults = await fetchToken.json();
 
-      if (!fetchTokenResults.access_token) throw new Error(fetchTokenResults.detail);
+        if (!fetchTokenResults.access_token)
+          throw new Error(fetchTokenResults.detail);
 
-      setToken(fetchTokenResults.access_token);
-    } catch (error) {
-      console.error(error);
-      setErrorMsg(error.message)
+        setToken(fetchTokenResults.access_token);
+      } catch (error) {
+        console.error(error);
+        setErrorMsg(error.message);
 
-      errorTimeout = setTimeout(() => setErrorMsg(null), 5000);
+        errorTimeout = setTimeout(() => setErrorMsg(null), 5000);
+      }
     }
-  });
+  );
 
   return generateToken;
 };
