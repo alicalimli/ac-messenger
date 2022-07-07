a907730aa41dfa80c3949523498175d446c5cb94
alicalimli <alicalimli76@gmail.com>
2022-07-07 13:30:56 +0300

    cleanup

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -4,18 +4,9 @@ import { useNavigate } from "react-router-dom";
 
 import { Sidebar, Conversation } from "../../Components";
 
-import { useLocalStorage, useAuth } from "../../Hooks";
+import { useLocalStorage } from "../../Hooks";
 
 const Home = () => {
-  const [userToken, setUserToken] = useLocalStorage("userToken", "");
-
-  const navigate = useNavigate();
-  const authenticate = useAuth();
-
-  useEffect(() => {
-    authenticate();
-  }, []);
-
   return (
     <>
       <Sidebar />
