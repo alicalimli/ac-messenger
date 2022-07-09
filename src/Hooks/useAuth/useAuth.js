bad73ce4932e20a681338ab15ede83ded9237cf3
alicalimli <alicalimli76@gmail.com>
2022-07-09 11:02:14 +0300

    create useGenerateToken again but revamped

--- src/Hooks/useAuth/useAuth.js
+++ src/Hooks/useAuth/useAuth.js
@@ -1,37 +1,20 @@
 import { useRef, useEffect, useState } from "react";
 
-const generateToken = async (email, pass) => {
-  let formData = new FormData();
-  formData.append("username", email);
-  formData.append("password", pass);
-
-  // Request login from the API
-  const fetchToken = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
-    method: "POST",
-    body: formData,
-  });
-
-  const fetchTokenResults = await fetchToken.json();
-
-  if (!fetchTokenResults.access_token)
-    throw new Error(fetchTokenResults.detail);
-
-  console.log(fetchTokenResults.access_token);
-
-  return fetchTokenResults.access_token;
-};
+import {useGenerateToken} from '../'
 
 const useAuth = () => {
   const [pendingMsg, setPendingMsg] = useState("");
 
+  const generateToken = useGenerateToken();
+
   const makeLogin = async (email, pass) => {
     console.log("generateToken");
     setPendingMsg("generating token");
-    const token = await generateToken(email, pass);
-    console.log("done");
+    const userToken = await generateToken(email,pass)
+    console.log(userToken)
     setPendingMsg("done");
 
-    return token;
+    return userToken;
   };
 
   return { makeLogin, pendingMsg };
---
+++ src/Hooks/useGenerateToken/useGenerateToken.js
@@ -0,0 +1,24 @@
+const useGenerateToken = () => {
+  const generateToken = async (email,pass) => {
+	  let formData = new FormData();
+	  formData.append("username", email);
+	  formData.append("password", pass);
+
+	  // Request login from the API
+	  const fetchToken = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
+	    method: "POST",
+	    body: formData,
+	  });
+
+	  const fetchTokenResults = await fetchToken.json();
+
+	  if (!fetchTokenResults.access_token)
+	    throw new Error(fetchTokenResults.detail);
+
+	 return fetchTokenResults.access_token;
+  }
+
+  return generateToken;
+};
+
+export default useGenerateToken;
