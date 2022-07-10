34b016cca1df0f089f304ba90d43c242701c667c
alicalimli <alicalimli76@gmail.com>
2022-07-10 10:17:33 +0300

    added show password button

--- src/Authentication/Components/InputForm/InputForm.jsx
+++ src/Authentication/Components/InputForm/InputForm.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+
 const InputForm = ({
   label,
   invalidLabel,
@@ -9,31 +12,54 @@ const InputForm = ({
   setState,
   minLength,
 }) => {
+  const [inputType, setInputType] = useState(type);
+
+  const handleShowPass = (e) => {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  };
+
   return (
     <label htmlFor={`${label}-input`} className="flex flex-col gap-2 relative">
       {label}
-      {isControlled ? (
-        <input
-          className="p-2 px-4 text-lg rounded-xl border border-slate-400 peer outline outline-2 invalid:outline-red-500 outline-blue-500/0 duration-200  hover:outline-blue-500 focus:outline-blue-500 bg-transparent"
-          type={type}
-          value={state}
-          ref={inputRef || null}
-          minLength={minLength}
-          placeholder={placeHolder}
-          id={label}
-          onChange={(e) => setState(e.target.value)}
-          onBlur={(e) => setState(e.target.value)}
-        />
-      ) : (
-        <input
-          className="p-2 px-4 text-lg rounded-xl border border-slate-400 peer outline outline-2 outline-blue-500/0 duration-200 hover:outline-blue-500 focus:outline-blue-500 bg-transparent"
-          type={type}
-          ref={inputRef}
-          minLength={minLength}
-          placeholder={placeHolder}
-          id={label}
-        />
-      )}
+      <div className="p-2 px-4 flex items-center rounded-xl border border-slate-400 peer outline outline-2 outline-blue-500/0 duration-200 hover:outline-blue-500 focus-within:outline-blue-500">
+        {isControlled ? (
+          <input
+            className="text-lg bg-transparent outline-none"
+            type={inputType}
+            value={state}
+            ref={inputRef || null}
+            minLength={minLength}
+            placeholder={placeHolder}
+            id={label}
+            onChange={(e) => setState(e.target.value)}
+            onBlur={(e) => setState(e.target.value)}
+          />
+        ) : (
+          <input
+            className="text-lg bg-transparent outline-none"
+            type={inputType}
+            ref={inputRef}
+            minLength={minLength}
+            placeholder={placeHolder}
+            id={label}
+          />
+        )}
+        {type === "password" ? (
+          <a className="cursor-pointer" onClick={handleShowPass}>
+            {inputType === "password" ? (
+              <BsFillEyeSlashFill className="text-slate-500 text-lg" />
+            ) : (
+              <BsFillEyeFill className="text-slate-500 text-lg" />
+            )}
+          </a>
+        ) : (
+          ""
+        )}
+      </div>
       <p className="hidden peer-invalid:block text-red-600 text-sm">
         {invalidLabel}
       </p>
