6ba365277bd002b4ca287b48c16493491d97617f
alicalimli <alicalimli76@gmail.com>
2022-07-10 16:09:48 +0300

    created tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: {
+          main: "#6741d9",
+          shaded: "#5f3dc4",
+          tinted: "#7048e8"
+        }
+      }
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
