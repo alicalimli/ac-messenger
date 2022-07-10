14b9240100d6aba328ddd7f7f59300d6dd4793ce
alicalimli <alicalimli76@gmail.com>
2022-07-10 09:46:53 +0300

    added animation when clicking signup and signin

--- src/Authentication/Authentication.jsx
+++ src/Authentication/Authentication.jsx
@@ -16,9 +16,10 @@ const Authentication = ({ setKeepSignedIn, keepSignedIn, setPendingMsg }) => {
       <AnimatePresence>
         {isSigningIn && (
           <motion.div
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.2 }}
-            initial={{ opacity: 0 }}
+            className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+            animate={{ opacity: 1, x:"-50%", y:"-50%"  }}
+            initial={{ opacity: 0, x:"-150%", y:"-50%"  }}
+            exit={{ opacity: 0, x:"-150%", y:"-50%"  }}
           >
             {" "}
             <SignIn
@@ -34,9 +35,10 @@ const Authentication = ({ setKeepSignedIn, keepSignedIn, setPendingMsg }) => {
       <AnimatePresence>
         {!isSigningIn && (
           <motion.div
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.2 }}
-            initial={{ opacity: 0 }}
+            className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+            animate={{ opacity: 1, x:"-50%", y:"-50%"  }}
+            initial={{ opacity: 0, x:"0", y:"-50%"  }}
+            exit={{ opacity: 0, x:"0%", y:"-50%"  }}
           >
             <SignUp
               setPendingMsg={setPendingMsg}
