9cc0955afea5bcbe9410ef689a713025cc54707d
alicalimli <alicalimli76@gmail.com>
2022-07-10 10:55:53 +0300

    style adjustments in input form

--- src/Authentication/Components/InputForm/InputForm.jsx
+++ src/Authentication/Components/InputForm/InputForm.jsx
@@ -24,11 +24,23 @@ const InputForm = ({
 
   return (
     <label htmlFor={`${label}-input`} className="flex flex-col gap-2 relative">
-      {label}
-      <div className="p-2 px-4 flex items-center rounded-xl border border-slate-400 peer outline outline-2 outline-blue-500/0 duration-200 hover:outline-blue-500 focus-within:outline-blue-500">
-        {isControlled ? (
+      <div className="flex flex-wrap gap-2 items-center">
+        <p>{label}</p>
+        {type === "password" ? (
+          <a className="cursor-pointer" onClick={handleShowPass}>
+            {inputType === "password" ? (
+              <BsFillEyeSlashFill className="text-slate-500 text-lg" />
+            ) : (
+              <BsFillEyeFill className="text-slate-500 text-lg" />
+            )}
+          </a>
+        ) : (
+          ""
+        )}
+      </div>
+      {isControlled ? (
           <input
-            className="text-lg bg-transparent outline-none w-full"
+            className="text-lg bg-transparent p-2 px-4 flex items-center rounded-xl border border-slate-400 peer outline outline-1 outline-blue-500/0 duration-200 hover:outline-blue-500 focus:outline-blue-500 invalid:outline-red-600"
             type={inputType}
             value={state}
             ref={inputRef || null}
@@ -40,7 +52,7 @@ const InputForm = ({
           />
         ) : (
           <input
-            className="text-lg bg-transparent outline-none w-full"
+            className="text-lg bg-transparent p-2 px-4 flex items-center rounded-xl border border-slate-400 peer outline outline-1 outline-blue-500/0 duration-200 hover:outline-blue-500 focus:outline-blue-500 invalid:outline-red-600"
             type={inputType}
             ref={inputRef}
             minLength={minLength}
@@ -48,18 +60,6 @@ const InputForm = ({
             id={label}
           />
         )}
-        {type === "password" ? (
-          <a className="ml-auto cursor-pointer" onClick={handleShowPass}>
-            {inputType === "password" ? (
-              <BsFillEyeSlashFill className="text-slate-500 text-lg" />
-            ) : (
-              <BsFillEyeFill className="text-slate-500 text-lg" />
-            )}
-          </a>
-        ) : (
-          ""
-        )}
-      </div>
       <p className="hidden peer-invalid:block text-red-600 text-sm">
         {invalidLabel}
       </p>
