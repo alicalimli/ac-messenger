9a19ab70bdce9bba13b5fff19ed74f6c39a06255
alicalimli <alicalimli76@gmail.com>
2022-07-11 11:22:02 +0300

    spacing adjustment

--- src/Components/Sidebar/SidebarBtn/SidebarBtn.jsx
+++ src/Components/Sidebar/SidebarBtn/SidebarBtn.jsx
@@ -5,7 +5,7 @@ const SidebarBtn = ({ children, clickHandler, name }) => {
 			className="relative p-2 text-muted text-2xl rounded group"
 		>
 			{children}
-			<span className="absolute shadow-md bg-gray-800 text-white rounded-xl p-2 px-4 text-sm invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-200 right-0 top-1/2 translate-x-full -translate-y-1/2">
+			<span className="absolute shadow-md bg-gray-800 text-white rounded-xl p-2 px-4 text-sm invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-200 -right-4 top-1/2 translate-x-full -translate-y-1/2">
 				{name}
 			</span>
 		</button>
