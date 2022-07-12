4c1c8ec31a075a417bc7caa42560d98a2e3f3d45
alicalimli <alicalimli76@gmail.com>
2022-07-12 15:34:18 +0300

    created different position options in tooltip

--- src/common/components/tw-tooltip/index.jsx
+++ src/common/components/tw-tooltip/index.jsx
@@ -4,6 +4,12 @@ function reducer(state, action) {
 	switch(action.type){
 		case 'right':
 			return '-right-4 top-1/2 translate-x-full -translate-y-1/2'
+		case 'left':
+			return '-left-4 top-1/2 -translate-x-full -translate-y-1/2'
+		case 'top':
+			return 'left-0 -top-full -translate-y-1/4'
+		case 'bottom':
+			return 'left-0 top-full translate-y-1/4'
 		default:
 			return ''
 	}
@@ -16,7 +22,7 @@ const TwTooltip = ({ children, addClass, position }) => {
 	useEffect(()=> dispatch({type: position}), [])
 
 	return (
-		<span className={`absolute shadow-md bg-gray-800 dark:bg-white text-white dark:text-black rounded-xl p-2 px-4 text-sm invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-200 z-10 ${positionClasses} `}>
+		<span className={`absolute shadow-md bg-gray-800 dark:bg-white text-white dark:text-black rounded-xl p-2 px-4 text-sm invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-200 z-10 pointer-events-none ${positionClasses} `}>
 			{children}
 		</span>
 	);
