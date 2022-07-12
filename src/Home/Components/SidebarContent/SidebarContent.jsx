d94542650799b268ead11c2953dc1b0c1b8dbac4
alicalimli <alicalimli76@gmail.com>
2022-07-12 07:53:04 +0300

    animation adjustments in sidebar content

--- src/Home/Components/SidebarContent/SidebarContent.jsx
+++ src/Home/Components/SidebarContent/SidebarContent.jsx
@@ -1,40 +1,38 @@
-import {Profile, Chats} from './Components'
+import { Profile, Chats } from "./Components";
 
-import {motion,AnimatePresence} from 'framer-motion'
+import { motion, AnimatePresence } from "framer-motion";
 
-const SidebarContent = ({sidebarContent, setSideBarContent}) => {
-  console.log(sidebarContent)
+const SidebarContent = ({ sidebarContent, setSideBarContent }) => {
+  console.log(sidebarContent);
   return (
-  	<div className="relative border-r border-muted-light/10 dark:border-muted-dark/10 w-full sm:w-[38rem] overflow-x-hidden">
-
+    <div className="relative border-r border-muted-light/10 dark:border-muted-dark/10 w-full sm:w-[38rem] overflow-x-hidden">
       <AnimatePresence>
-        {sidebarContent === "profile" &&
-        <motion.div
-        className="absolute w-full h-full"
-        animate={{opacity:1, x:0}}
-        initial={{opacity:0, x:'50%'}}
-        exit={{opacity:0, x:'-50%'}}
-        transition={{duration:0.2}}
-        >
-          <Profile />
-        </motion.div>
-      }
+        {sidebarContent === "chats" && (
+          <motion.div
+            key="chats"
+            className="absolute w-full h-full"
+            animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: "-50%" }}
+            exit={{ opacity: 0, x: "50%" }}
+              transition={{type:'spring', mass:0.25}}
+          >
+            <Chats />
+          </motion.div>
+        )}
+        {sidebarContent === "profile" && (
+          <motion.div
+            key="profile"
+            className="absolute w-full h-full"
+            animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: "-50%" }}
+            exit={{ opacity: 0, x: "50%" }}
+              transition={{type:'spring', mass:0.25}}
+          >
+            <Profile />
+          </motion.div>
+        )}
       </AnimatePresence>
-
-      <AnimatePresence>
-        {sidebarContent === "chats" &&
-        <motion.div
-        className="absolute w-full h-full"
-        animate={{opacity:1, x:0}}
-        initial={{opacity:0, x:'50%'}}
-        exit={{opacity:0, x:'-50%'}}
-        transition={{duration:0.2}}
-        >
-          <Chats />
-        </motion.div>
-      }
-      </AnimatePresence>
-  	</div>
+    </div>
   );
 };
 export default SidebarContent;
