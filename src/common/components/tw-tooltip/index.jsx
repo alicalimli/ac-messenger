82084b282d61d80c54a99ae68210dd49e2b5afc6
alicalimli <alicalimli76@gmail.com>
2022-07-12 14:33:19 +0300

    name fix

--- src/common/components/tw-tooltip/index.jsx
+++ src/common/components/tw-tooltip/index.jsx
@@ -1,4 +1,4 @@
-const TwButton = ({ children, addClass }) => {
+const TwTooltip = ({ children, addClass }) => {
 	return (
 		<span className="absolute shadow-md bg-gray-800 dark:bg-white text-white dark:text-black rounded-xl p-2 px-4 text-sm invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-200 -right-4 top-1/2 translate-x-full -translate-y-1/2 z-10">
 			{children}
@@ -6,4 +6,4 @@ const TwButton = ({ children, addClass }) => {
 	);
 };
 
-export default TwButton;
+export default TwTooltip;
