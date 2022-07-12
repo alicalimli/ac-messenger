e653a38fd429f6b160f385fcdfff13bed50429ab
alicalimli <alicalimli76@gmail.com>
2022-07-12 14:32:57 +0300

    created tooltip component

---
+++ src/common/components/tw-tooltip/index.jsx
@@ -0,0 +1,9 @@
+const TwButton = ({ children, addClass }) => {
+	return (
+		<span className="absolute shadow-md bg-gray-800 dark:bg-white text-white dark:text-black rounded-xl p-2 px-4 text-sm invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-200 -right-4 top-1/2 translate-x-full -translate-y-1/2 z-10">
+			{children}
+		</span>
+	);
+};
+
+export default TwButton;
