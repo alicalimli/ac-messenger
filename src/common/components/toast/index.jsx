59298f04cff2764b645eb961c72f49c6be45bb2b
alicalimli <alicalimli76@gmail.com>
2022-07-12 17:53:17 +0300

    bug fix

--- src/common/components/toast/index.jsx
+++ src/common/components/toast/index.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
+let toastTimeout;
+
 const Toast = ({ children, durationMs, msg, setMsg }) => {
-  let toastTimeout;
 
   const removeMsg = () => {
     if (!durationMs || typeof +durationMs !== "number") return;
-
-    clearTimeout(toastTimeout);
+    clearTimeout(toastTimeout)
     toastTimeout = setTimeout(() => setMsg(""), +durationMs);
   };
 
@@ -23,7 +23,6 @@ const Toast = ({ children, durationMs, msg, setMsg }) => {
           key="msg-toast"
           animate={{ y: 0, x: "-50%" }}
           exit={{ y: -100, x: "-50%" }}
-          transition={{ duration: 0.3 }}
           initial={{ y: -100, x: "-50%" }}
           className="bg-white mt-4 fixed top-0 left-1/2 flex items-center justify-center z-10 shadow-md p-4 px-8 rounded-xl"
         >
