3402cd2f9a53928fe56a65a887ea8946eaf1a9b6
alicalimli <alicalimli76@gmail.com>
2022-07-12 17:07:29 +0300

    added hover and active animation in buttons

--- src/common/components/tw-button/index.jsx
+++ src/common/components/tw-button/index.jsx
@@ -1,13 +1,17 @@
+import {motion} from 'framer-motion'
+
 const TwButton = ({children, isDisabled, addClass, clickHandler}) => {
 	return (
-        <button
+        <motion.button
+        whileHover={{scale:1.03}}
+        whileTap={{scale:0.98}}
         onClick={clickHandler || null}
         disabled={isDisabled ? true : false}
-          className={`bg-primary-main hover:bg-primary-tinted duration-300
+          className={`bg-primary-main hover:bg-primary-tinted
           	rounded-xl p-2 px-4 text-white ${addClass}`}
         >
           {children}
-        </button>
+        </motion.button>
 		);
 }
 
--- src/common/components/tw-trn-button/index.jsx
+++ src/common/components/tw-trn-button/index.jsx
@@ -1,11 +1,15 @@
+import {motion} from 'framer-motion'
+
 const TwTrnButton = ({children, isDisabled, addClass, clickHandler}) => {
 	return (
-        <button
+        <motion.button
+        whileHover={{scale:1.03}}
+        whileTap={{scale:0.98}}
         onClick={clickHandler || null}
         disabled={isDisabled ? true : false}
-        className={`flex gap-4 items-center text-lg hover:bg-muted-light/10 p-2 text-md rounded-xl dark:hover:bg-muted-dark/10 duration-200 text-black dark:text-white ${addClass}`}>
+        className={`flex gap-4 items-center text-lg hover:bg-muted-light/10 p-2 text-md rounded-xl dark:hover:bg-muted-dark/10 text-black dark:text-white ${addClass}`}>
         {children}
-        </button>
+        </motion.button>
 		);
 }
 
