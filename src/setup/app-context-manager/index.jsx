7b6e1ecbdfb7511fc83e05ccd82f8503745753f3
alicalimli <alicalimli76@gmail.com>
2022-07-14 09:11:29 +0300

    created toast message context

--- src/setup/app-context-manager/index.jsx
+++ src/setup/app-context-manager/index.jsx
@@ -2,15 +2,19 @@ import React, { useState, createContext } from "react";
 
 export const UserContext = createContext();
 export const UserTokenContext = createContext();
+export const ToastMsgContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [userToken, setUserToken] = useState("");
+  const [toastMsg, setToastMsg] = useState('');
 
   return (
     <UserContext.Provider value={[user, setUser]}>
       <UserTokenContext.Provider value={[userToken, setUserToken]}>
-        {children}
+        <toastMsg value={[toastMsg,setToastMsg]}>
+          {children}
+        </toastMsg>
       </UserTokenContext.Provider>
     </UserContext.Provider>
   );
