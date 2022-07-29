3ed860443b4617b3b5bda3d176d1da98d41cbbfb
alicalimli <alicalimli76@gmail.com>
2022-07-29 17:57:13 +0300

    added a guide when user hasn't click inbox chat yet

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,6 +9,8 @@ import { ChatBox } from "../../features/conversation/components";
 import {CurrentChatContext} from '/src/setup/app-context-manager'
 import { VARIANTS_MANAGER } from "/src/setup/variants-manager";
 
+import {start_messaging_img} from '/src/assets/images/'
+
 const Home = () => {
   const defaultSideBarContent = "chats";
   const [sidebarContent, setSideBarContent] = useState(defaultSideBarContent);
@@ -29,13 +31,24 @@ const Home = () => {
         setSideBarContent={setSideBarContent}
         previousContentRef={previousContentRef}
       />
-      { currentChat && (
       <div
-        className="h-screen w-screen justify-center hidden md:block bg-muted-light/10 dark:bg-black duration-300"
+        className="h-screen w-screen justify-center hidden bg-muted-light/10 dark:bg-black duration-300 md:flex items-center justify-center"
       >
-        <ChatBox currentChat={currentChat} />
+        { currentChat ? (
+          <ChatBox currentChat={currentChat} />
+          ) :
+        <motion.div
+        className="flex flex-col gap-4 items-center justify-center"
+            variants={VARIANTS_MANAGER}
+            initial="pop-out"
+            animate="pop-in"
+            exit="pop-out"
+        >
+              <img src={start_messaging_img} className="w-full md:w-96" />
+              <h1 className="text-black dark:text-white text-2xl max-w-md text-center">Select a chat in your inbox to start messaging.</h1>
+        </motion.div>
+      }
       </div>
-        ) }
     </div>
   );
 };
