cd0b2519f3c3b74a27b998c14a8950cee9780802
alicalimli <alicalimli76@gmail.com>
2022-07-29 11:14:59 +0300

    handle inbox click

--- src/features/inbox/components/InboxList.jsx
+++ src/features/inbox/components/InboxList.jsx
@@ -1,27 +1,36 @@
+import {useContext} from 'react'
+
 import elvis from "/src/assets/images/elvis.jpg";
 
 import { TwTrnButton } from "/src/components";
 import { useGetInboxList } from "../hooks";
 
+import {CurrentChatContext} from '/src/setup/app-context-manager'
+
 const InboxList = () => {
   const inboxLists = useGetInboxList();
+  const [currentChat, setCurrentChat] = useContext(CurrentChatContext)
+
+  const inboxListClickHandler = (inboxList) => {
+    setCurrentChat(inboxList)
+  }
 
   return (
     <div className=" p-4 flex flex-col gap-4">
       <h1 className="text-black dark:text-white text-2xl">Chats</h1>
       <div>
-        {inboxLists.map((list, i) => (
-          <TwTrnButton addClass="w-full p-2" key={list.sender_name + i}>
+        {inboxLists.map((inboxList, i) => (
+          <TwTrnButton clickHandler={() => inboxListClickHandler(inboxList)} addClass="w-full p-2" key={inboxList.sender_name + i}>
             <div className="relative bg-transparent h-16 w-16">
               <div className="bg-green-500 p-2 rounded-full absolute right-1 bottom-0"></div>
               <img src={elvis} className="w-full rounded-full" />
             </div>
             <div className="flex flex-col items-start">
               <h2 className="text-xl text-black dark:text-white">
-                {list.sender_name}
+                {inboxList.sender_name}
               </h2>
               <p className="text-sm text-muted-light dark:text-muted-dark">
-                {list.last_message}
+                {inboxList.last_message}
               </p>
             </div>
           </TwTrnButton>
