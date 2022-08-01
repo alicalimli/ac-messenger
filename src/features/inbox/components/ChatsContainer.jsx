dc9306c971195774c2727dbcac4e0e87b4016678
alicalimli <alicalimli76@gmail.com>
2022-08-01 10:45:19 +0300

    renamings

--- src/features/inbox/components/ChatsContainer.jsx
+++ src/features/inbox/components/ChatsContainer.jsx
@@ -8,30 +8,30 @@ const ChatsContainer = () => {
   const chats = useGetChats();
   const [currentChat, setCurrentChat] = useContext(CurrentChatContext);
 
-  const chatClickHandler = (inboxList) => {
-    setCurrentChat(inboxList);
+  const chatClickHandler = (chat) => {
+    setCurrentChat(chat);
   };
 
   return (
     <div className=" p-4 flex flex-col gap-4">
       <h1 className="text-black dark:text-white text-2xl">Chats</h1>
       <div>
-        {chats.map((inboxList, i) => (
+        {chats.map((chat, i) => (
           <TwTrnButton
-            clickHandler={() => chatClickHandler(inboxList)}
+            clickHandler={() => chatClickHandler(chat)}
             addClass="w-full p-2"
-            key={inboxList.sender_name + i}
+            key={chat.sender_name + i}
           >
             <div className="relative bg-transparent h-16 w-16">
               <div className="bg-green-500 p-2 rounded-full absolute right-1 bottom-0"></div>
-              <img src={inboxList.profile} className="w-full rounded-full" />
+              <img src={chat.profile} className="w-full rounded-full" />
             </div>
             <div className="flex flex-col items-start">
               <h2 className="text-xl text-black dark:text-white">
-                {inboxList.sender_name}
+                {chat.sender_name}
               </h2>
               <p className="text-sm text-muted-light dark:text-muted-dark">
-                {inboxList.last_message}
+                {chat.last_message}
               </p>
             </div>
           </TwTrnButton>
