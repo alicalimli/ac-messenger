4ec602caf1f5bda860fddea1a042523e1d6c9bb9
alicalimli <alicalimli76@gmail.com>
2022-08-02 20:17:24 +0300

    render users in the add contacts page

--- src/features/add-contacts/components/AddContacts.jsx
+++ src/features/add-contacts/components/AddContacts.jsx
@@ -1,13 +1,26 @@
 import { TwTrnButton } from "/src/components";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
+import { useGetUsers } from '/src/hooks'
+
 const AddContacts = ({ previousContentRef, setSideBarContent }) => {
+  const users = useGetUsers();
+
   return (
     <section className="flex flex-col items-center p-4 px-8">
         <TwTrnButton
           clickHandler={() => setSideBarContent(previousContentRef.current)}
           addClass="w-full flex gap-2"
         ><AiOutlineArrowLeft className="text-lg" /> Add Contacts</TwTrnButton>
+        {users.map((user) => (
+           <TwTrnButton addClass="w-full flex gap-4">
+            <div className="relative bg-transparent h-12 w-12">
+              {user.active && <div className="bg-green-500 p-1.5 rounded-full absolute right-1 bottom-0"></div>}
+              <img src={user.profile} className="w-full rounded-full" />
+            </div>
+            {user.username}
+          </TwTrnButton>
+        ))}
     </section>
   );
 };
