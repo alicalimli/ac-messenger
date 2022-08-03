b20eabb46e53da8d7c59309b7679f389e91f30fe
alicalimli <alicalimli76@gmail.com>
2022-08-03 09:52:43 +0300

    handled contact user click

--- src/features/add-contacts/components/AddContacts.jsx
+++ src/features/add-contacts/components/AddContacts.jsx
@@ -1,19 +1,52 @@
+
+import {useState, useContext} from 'react'
 import { TwTrnButton } from "/src/components";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
+import { Modal } from '/src/components'
 import { useGetUsers } from '/src/hooks'
+
+import {UserContext} from '/src/setup/app-context-manager'
 
 const AddContacts = ({ setSideBarContent }) => {
   const users = useGetUsers();
+  const [userInfo, setUserInfo] = useContext(UserContext);
+  const [otherUser, setOtherUser] = useState();
+  const [showModal, setShowModal] = useState();
+  console.log(users)
+
+  const contactClickHandler = (otherUser) => {
+    setShowModal(true)
+    setOtherUser(otherUser)
+  };
 
   return (
     <section className="flex flex-col items-center p-4 px-8">
+        <Modal setShowModal={setShowModal}>
+          {showModal &&
+            <div className="flex flex-col items-center text-center p-4 px-8">
+              <img
+                className="bg-cover bg-center bg-transparent mb-2 w-16 h-16 rounded-full shadow-md"
+                alt={`${otherUser.username}'s profile picture`}
+                src={otherUser.profile}
+              />
+              <h2 className="text-lg text-black dark:text-white">
+                {otherUser.username}
+              </h2>
+              <p className="text-muted-light dark:text-muted-dark">
+                Front-end Developer
+              </p>
+            </div>
+          }
+        </Modal>
+
         <TwTrnButton
           clickHandler={() => setSideBarContent("chats")}
           addClass="w-full flex gap-2"
         ><AiOutlineArrowLeft className="text-lg" /> Add Contacts</TwTrnButton>
-        {users.map((user) => (
-           <TwTrnButton addClass="w-full flex gap-4">
+
+        {users.map((user, i) => (
+           <TwTrnButton key={i} clickHandler={() => contactClickHandler(user)} addClass="w-full flex gap-4">
             <div className="relative bg-transparent h-12 w-12">
               {user.active && <div className="bg-green-500 p-1.5 rounded-full absolute right-1 bottom-0"></div>}
               <img src={user.profile} className="w-full rounded-full" />
