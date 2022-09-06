7870ec33a8cfbb782d517e3c7111a18f54dc2e81
alicalimli <alicalimli76@gmail.com>
2022-09-06 12:48:11 +0300

    typescript installation fixes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const root = document.getElementById("root");
+const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.render(React.createElement(App), root);
