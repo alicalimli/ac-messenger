a98a71e1a273c56b84303886c1cc1a1042ad929f
alicalimli <alicalimli76@gmail.com>
2022-09-10 11:49:38 +0300

    created userslice

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AppContextProvider } from "setup/app-context-manager";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "features/authentication/reducers/user";
+
+const store = configureStore({
+  reducer: { userReducer },
+});
 
 const root = document.getElementById("root") as HTMLElement;
 
 const AppMock = (): JSX.Element => {
   return (
     <AppContextProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AppContextProvider>
   );
 };
