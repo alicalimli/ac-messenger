ffee28c1fa189b6756f2c4470d059e85d19ffe97
alicalimli <alicalimli76@gmail.com>
2022-09-12 15:24:43 +0300

    typed useSignout hook

--- src/hooks/use-sign-out/useSignOut.ts
+++ src/hooks/use-sign-out/useSignOut.ts
@@ -1,18 +1,9 @@
-import { useContext } from "react";
 import { useDispatch } from "react-redux";
-import { logout } from "/src/features/authentication";
+import { logout } from "features/authentication";
+import { useLocalStorage } from "hooks";
 
-import {
-  CurrentChatContext,
-  UserTokenContext,
-} from "/src/setup/app-context-manager";
-
-import { useLocalStorage } from "/src/hooks";
-
-const useSignOut = (key, initialValue) => {
+const useSignOut = () => {
   const dispatch = useDispatch();
-  const [userToken, setUserToken] = useContext(UserTokenContext);
-  const [currentChat, setCurrentChat] = useContext(CurrentChatContext);
 
   const [savedUserInfo, setSavedUserInfo] = useLocalStorage("userInfo", null);
   const [savedUserToken, setSavedUserToken] = useLocalStorage("userToken", "");
@@ -23,10 +14,8 @@ const useSignOut = (key, initialValue) => {
 
   const signOut = () => {
     dispatch(logout());
-    setUserToken("");
     setSavedUserInfo(null);
     setSavedUserToken("");
-    setCurrentChat(null);
     setKeepSignedIn(false);
   };
 
