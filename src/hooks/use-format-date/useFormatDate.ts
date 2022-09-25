7a4d8d1a8cad3ce1c2a7b813ef9cce6be40aebe5
alicalimli <alicalimli76@gmail.com>
2022-09-25 09:56:13 +0300

    created useFormatDate hook

---
+++ src/hooks/use-format-date/useFormatDate.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { isSameDay, isSameWeek, isSameYear, format, addDays } from "date-fns";
+
+const useGetUsers = (date: Date) => {
+  const [formattedDate, setFormattedDate] = useState<string>();
+
+  useEffect(() => {
+    if (!date) return;
+
+    if (isSameDay(date, new Date())) {
+      setFormattedDate(format(date, "h:mmbbb"));
+    } else if (isSameWeek(date, new Date())) {
+      setFormattedDate(format(date, "iii h:mmbbb"));
+    } else if (isSameYear(date, new Date())) {
+      setFormattedDate(format(date, "LLL dd"));
+    } else {
+      setFormattedDate(format(date, "LLL dd yyy"));
+    }
+  }, []);
+
+  return formattedDate;
+};
+
+export default useGetUsers;
