1c5dcf5dc3bc554f709e44b1f4a218b5bdddab18
alicalimli <alicalimli76@gmail.com>
2022-10-06 12:27:14 +0300

    [ADD] fade animation in loading spinner

--- src/components/loading-spinner/LoadingSpinner.tsx
+++ src/components/loading-spinner/LoadingSpinner.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { VARIANTS_MANAGER } from "setup/variants-manager";
 
 interface LoadingSpinnerProps {
   className?: string;
@@ -7,12 +9,15 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner = ({ className, msg }: LoadingSpinnerProps) => {
   return (
-    <div
+    <motion.div
+      variants={VARIANTS_MANAGER}
+      animate="fade-in"
+      initial="fade-out"
       className={`${className} flex flex-col justify-center items-center gap-2`}
     >
       <AiOutlineLoading3Quarters className="animate-spin text-3xl  text-primary-main" />
       <p className="text-black dark:text-white">{msg}</p>
-    </div>
+    </motion.div>
   );
 };
 
