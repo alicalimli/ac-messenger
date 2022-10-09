d12fed1e6b1e4ab1399ab04989165a3d19239753
alicalimli <alicalimli76@gmail.com>
2022-10-09 15:39:07 +0300

    [REM] derivedStateError param

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component<Props, State> {
     hasError: false,
   };
 
-  public static getDerivedStateFromError(_: Error): State {
+  public static getDerivedStateFromError(): State {
     return { hasError: true };
   }
 
