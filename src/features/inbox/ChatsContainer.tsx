6f43e95824418eed77a13ded6d250d127abb9c2c
alicalimli <alicalimli76@gmail.com>
2022-10-09 13:54:25 +0300

    [IMP] styles

--- src/features/inbox/ChatsContainer.tsx
+++ src/features/inbox/ChatsContainer.tsx
@@ -37,8 +37,8 @@ const ChatsContainer = () => {
     };
   }, [currentUser.uid]);
   return (
-    <div className="p-4 flex flex-col gap-4 h-full">
-      <h1 className="text-black dark:text-white text-2xl">Chats</h1>
+    <div className="p-4 flex flex-col gap-2 h-full">
+      <h1 className="ml-4 text-black dark:text-white text-2xl">Chats</h1>
       <div className="relative flex flex-col overflow-scroll scrollbar-hide">
         {Object.entries(chats).length !== 0 &&
           Object.entries(chats)
