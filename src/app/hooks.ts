993ac12171da9af60b84790aae56bb3e17b0d5a7
alicalimli <alicalimli76@gmail.com>
2022-10-10 14:46:21 +0300

    [IMP] moved store file to setup folder

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from "app/store";
+import { RootState, AppDispatch } from "setup/store";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "app/store";
+import store from "setup/store";
 
 import { Provider } from "react-redux";
 import ErrorBoundary from "ErrorBoundary";
