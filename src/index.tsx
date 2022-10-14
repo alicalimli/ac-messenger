e67bb167f61265a777895b92612bbee40c50cd1e
alicalimli <alicalimli76@gmail.com>
2022-10-14 14:30:29 +0300

    [ADD] persist darkmode in local storage

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,21 @@ import store from "setup/store";
 
 import { Provider } from "react-redux";
 import ErrorBoundary from "pages/ErrorBoundary";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = document.getElementById("root") as HTMLElement;
+
+let persistor = persistStore(store);
 
 const AppMock = (): JSX.Element => {
   return (
     <Provider store={store}>
       <ErrorBoundary>
+      <PersistGate persistor={persistor}>
+
         <App />
+      </PersistGate>
       </ErrorBoundary>
     </Provider>
   );
--- src/setup/store/index.ts
+++ src/setup/store/index.ts
@@ -1,17 +1,31 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "features/authentication";
 import chatReducer from "features/inbox/chatReducer";
 import { themeReducer } from "features/sidebar";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/es/storage";
 import toastReducer from "toastSlice";
 
-const store = configureStore({
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  blacklist: ["user", "chat", "toast"],
+};
 
-reducer: {
-    user: userReducer,
-    theme: themeReducer,
-    toast: toastReducer,
-    chat: chatReducer,
-  },
+const reducer = combineReducers({
+  theme: themeReducer,
+  user: userReducer,
+  chat: chatReducer,
+  toast: toastReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+console.log(persistedReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
