9a754ea9e4b2055f5c393495b8a86c476205a214
alicalimli <alicalimli76@gmail.com>
2022-10-26 12:34:03 +0300

    [ADD] comments in firebase config

--- src/setup/firebase/index.ts
+++ src/setup/firebase/index.ts
@@ -14,4 +14,29 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const googleAuthProvider = new GoogleAuthProvider();
 
+// Creates a global test group to play around with once.
 createGlobalChat(db);
+
+// Firestore rule
+
+// rules_version = '2';
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+//     match /users/{userId} {
+//       allow create
+//       allow read, write : if request.auth.uid != null
+//     }
+
+//     match /userChats/{userId} {
+//       allow read, write : if request.auth.uid != null
+//     }
+
+//     match /chats/{chat} {
+//       allow read, write : if request.auth.uid != null
+//     }
+
+//     match /groupChats/{groupChat} {
+//       allow read, write : if request.auth.uid != null
+//     }
+//   }
+// }
